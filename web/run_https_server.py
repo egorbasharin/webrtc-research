e5e54952ece5bef343e8e503118de1980453f585
Egor <60278808+egorbasharin@users.noreply.github.com>
2022-07-16 16:15:23 +0300

    Fix paths to pem

--- web/run_https_server.py
+++ web/run_https_server.py
@@ -5,7 +5,7 @@ import ssl
 httpd = HTTPServer(('localhost', 4443), BaseHTTPRequestHandler)
 
 httpd.socket = ssl.wrap_socket (httpd.socket, 
-        keyfile="path/to/key.pem", 
-        certfile='path/to/cert.pem', server_side=True)
+        keyfile="./key.pem", 
+        certfile='./cert.pem', server_side=True)
 
 httpd.serve_forever()
