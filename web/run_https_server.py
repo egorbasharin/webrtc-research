aeb4c59e09d5c8640835972ea5b69efa5e07a437
Egor <60278808+egorbasharin@users.noreply.github.com>
2022-07-16 16:14:43 +0300

    Use py3 version for https

--- web/run_https_server.py
+++ web/run_https_server.py
@@ -1,12 +1,11 @@
-import BaseHTTPServer, SimpleHTTPServer
+from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', 4443),
-        SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd = HTTPServer(('localhost', 4443), BaseHTTPRequestHandler)
 
-httpd.socket = ssl.wrap_socket (httpd.socket,
-        keyfile="key.pem",
-        certfile='cert.pem', server_side=True)
+httpd.socket = ssl.wrap_socket (httpd.socket, 
+        keyfile="path/to/key.pem", 
+        certfile='path/to/cert.pem', server_side=True)
 
 httpd.serve_forever()
